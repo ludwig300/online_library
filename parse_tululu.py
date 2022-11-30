fe151ef7f7268c455808c340a500a885e33f1763
ludwig300 <105148929+ludwig300@users.noreply.github.com>
2022-11-30 20:30:50 +0700

    Renamed file and added `argparse`

--- parse_tululu.py
+++ parse_tululu.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 
 from bs4 import BeautifulSoup
@@ -20,9 +21,12 @@ def parse_book_page(soup):
         soup.find(class_='bookimage').find('img')['src']
     )
     comments = [comment.text for comment in soup.select('.texts .black')]
-    genres = [genre.text for genre in soup.select_one('.d_book:-soup-contains("Жанр книги:")').find_all('a')]
+    genres = [genre.text for genre in soup.select_one(
+        '.d_book:-soup-contains("Жанр книги:")'
+    ).find_all('a')]
     return {
         'title': title.strip(),
+        'author': author.strip(),
         'image_url': image_url,
         'comments': comments,
         'genres': genres
@@ -74,22 +78,45 @@ def download_image(url, filename, folder='images/'):
         file.write(response.content)
 
 
+def create_parser():
+    parser = argparse.ArgumentParser(description='Get "group_id"')
+    parser.add_argument(
+        '--start_id',
+        default=1,
+        help='Book id. Default 1',
+        type=int
+    )
+    parser.add_argument(
+        '--end_id',
+        default=10,
+        help='Book id. Default 10',
+        type=int
+    )
+    return parser
+
+
 def main():
+    parser = create_parser()
+    args = parser.parse_args()
     url = "https://tululu.org/txt.php"
-    for book_id in range(1, 11):
+    for book_id in range(args.start_id, args.end_id):
         page_url = f"https://tululu.org/b{book_id}/"
         page_response = requests.get(page_url)
-        page_soup = BeautifulSoup(page_response.text, 'lxml')
+        page_response.raise_for_status()
         payload = {'id': book_id}
         response = requests.get(url, params=payload)
         response.raise_for_status()
         try:
             check_for_redirect(response)
+            check_for_redirect(page_response)
+            page_soup = BeautifulSoup(page_response.text, 'lxml')
             book_page = parse_book_page(page_soup)
             image_url = book_page['image_url']
             title = book_page['title']
             filename = f'{book_id}.{title}.txt'
             filename_img = f'{book_id}{get_extension(image_url)}'
+            print('Название:', book_page['title'])
+            print('Автор:', book_page['author'])
             download_txt(response.url, filename)
             download_image(image_url, filename_img)
 
