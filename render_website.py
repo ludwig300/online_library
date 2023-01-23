80c3b41cb20dfcfec56dc7b83443da3b47092cac
ludwig300 <105148929+ludwig300@users.noreply.github.com>
2023-01-23 21:33:10 +0700

    Integrate livereload

--- render_website.py
+++ render_website.py
@@ -1,32 +1,28 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import json
+from livereload import Server
 
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
+def on_reload():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    template = env.get_template('template.html')
+    with open("book_description.json", "r", encoding="utf8") as book_file:
+        books_descriptions_json = book_file.read()
+    books_descriptions = json.loads(books_descriptions_json)
+    rendered_page = template.render(books=books_descriptions)
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
 
-template = env.get_template('template.html')
-# rendered_page = template.render(
-#     cap1_title="Красная кепка",
-#     cap1_text="$ 100.00",
-#     cap2_title="Чёрная кепка",
-#     cap2_text="$ 120.00",
-#     cap3_title="Ещё одна чёрная кепка",
-#     cap3_text="$ 90.00",
-# )
-with open("book_description.json", "r", encoding="utf8") as book_file:
-    books_descriptions_json = book_file.read()
 
-books_descriptions = json.loads(books_descriptions_json)
-print(books_descriptions)
-rendered_page = template.render(books=books_descriptions)
+def main():
+    on_reload()
+    server = Server()
+    server.watch('template.html', on_reload)
+    server.serve(root='.')
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+if __name__ == '__main__':
+    main()
